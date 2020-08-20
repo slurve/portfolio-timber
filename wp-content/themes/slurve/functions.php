7c6a1478eec81579f3cd3f481c07796a3f0ba3cc
Tom Rose <tom@slurve.com>
2020-08-20 16:42:33 -0400

    sync styles

--- wp-content/themes/slurve/functions.php
+++ wp-content/themes/slurve/functions.php
@@ -71,59 +71,6 @@ function form_submit_button($button, $form)
   return "<button class='btn btn-primary btn-block' id='gform_submit_button_{$form['id']}'><span>Submit</span></button>";
 }
 
-/* Hover Module Effect */
-function my_acf_admin_head()
-{
-  $siteURL = get_site_url(); ?>
-  <style type="text/css">
-    .imagePreview {
-      position: absolute;
-      right: 100%;
-      top: 0px;
-      z-index: 999999;
-      border: 1px solid #fff;
-      box-shadow: 0px 10px 13px #b6b6b6;
-      background-color: #fff;
-      padding: 30px;
-    }
-
-    .imagePreview img {
-      width: 480px;
-      height: auto;
-      display: block; }
-
-    .acf-tooltip li:hover {
-      background-color: #0e6fcc;
-    }
-  </style>
-  <script>
-    jQuery(document).ready(function($) {
-      $('a[data-name=add-layout]').click(function(){
-        waitForEl('.acf-tooltip li', function() {
-          $('.acf-tooltip li a').hover(function(){
-            imageTP = $(this).attr('data-layout');
-            imageTP = imageTP.replace(/_/g,"-");
-            $('.acf-tooltip').append('<div class="imagePreview"><img src="<?php echo $siteURL; ?>/wp-content/themes/samcart/templates/blocks/screenshots/' + imageTP + '.png"></div>');
-          }, function(){
-            $('.imagePreview').remove();
-          });
-        });
-      });
-      var waitForEl = function(selector, callback) {
-        if (jQuery(selector).length) {
-          callback();
-        } else {
-          setTimeout(function() {
-            waitForEl(selector, callback);
-          }, 100);
-        }
-      };
-    });
-  </script>
-  <?php
-}
-add_action('acf/input/admin_head', 'my_acf_admin_head');
-
 //Page Slug Body Class
 function add_slug_body_class($classes)
 {
