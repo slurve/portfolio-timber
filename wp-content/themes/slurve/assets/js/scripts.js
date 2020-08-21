22f09f054c6dcb61a9ba8670a13cbbfd08f2b4a1
Tom Rose <tom@slurve.com>
2020-08-21 15:34:38 -0400

    more styles

--- wp-content/themes/slurve/assets/js/scripts.js
+++ wp-content/themes/slurve/assets/js/scripts.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($) {
   // $(window).scroll(function() {
   //   var y = $(window).scrollTop();
-  //   var d = y / 16;
-  //   $(".logos").css("transform", "translate3d(0," + d + "px, 0)");
+  //   var d = y / 32;
+  //   $(".block-hero picture:first-child").css(
+  //     "transform",
+  //     "translate3d(0," + d + "px, 0)"
+  //   );
   // });
 });
