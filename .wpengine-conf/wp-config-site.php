506969c023dd2508bf20e5641634662c0576a0fc
Tom Rose <tom@slurve.com>
2020-09-24 12:03:29 -0400

    mobile tweaks

---
+++ .wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2020-09-24 09:41:05.455781 -0400 EDT m=+109.459899779
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "slurve.wpengine.com";
+$wpengine_primary_original_ip_address = "104.196.23.135";
